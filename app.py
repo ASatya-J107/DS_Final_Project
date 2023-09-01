b14343e831b135a55eabd078095083d928f18742
ASatya-J107 <143426821+ASatya-J107@users.noreply.github.com>
2023-09-02 02:13:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pickle
 import pandas as pd
-import scikit-learn
+import sklearn
 
 with open('model/elastic_math_model.pkl','rb') as file:
     elastic_math_model = pickle.load(file)
